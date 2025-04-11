61c3ef7d6f1e114c339fd54581cf4c1cc895ddca
Linka <Polina153@mail.ru>
2025-04-11 18:00:40 +0400

    Navigation View Drawer View Binding Toolbar

--- app/src/main/java/com/example/javasecondactivity/MainActivity.java
+++ app/src/main/java/com/example/javasecondactivity/MainActivity.java
@@ -1,43 +1,107 @@
 package com.example.javasecondactivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.javasecondactivity.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button button;
+    //EditText editText;
+    //Button button;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        editText = findViewById(R.id.editTextText);
-        button = findViewById(R.id.sendStringButton);
+        //Toolbar myToolbar = findViewById(R.id.toolbar);
+        //myToolbar.inflateMenu(R.menu.menu);
+        setSupportActionBar(binding.toolbar);
+        initDrawer(binding.toolbar);
+        //editText = findViewById(R.id.editTextText);
+        //button = findViewById(R.id.sendStringButton);
 
-        
-        button.setOnClickListener(new View.OnClickListener() {
+        binding.sendStringButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                String stringFromUser = editText.getText().toString();
+                String stringFromUser = binding.editTextText.getText().toString();
                 intent.putExtra("stringFromUser", stringFromUser);
                 startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        if (item.getItemId() == R.id.some_action) {
+            Toast.makeText(this, "menu button pressed", Toast.LENGTH_LONG).show();
+            return true;
+        } else{
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    private void initDrawer(Toolbar toolbar) {
+
+        // Находим DrawerLayout
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // Создаем ActionBarDrawerToggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id== R.id.some_action) {
+                    Toast.makeText(getApplicationContext(), "menu button pressed", Toast.LENGTH_LONG).show();
+                        return true;
+                }
+                return false;
             }
         });
     }
